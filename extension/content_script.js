5de2d82ce61bde62c5d0901eec579cbe64933df2
Ethan Pippin <ethanpippin2343@gmail.com>
2020-05-31 09:01:13 -0600

    add social distancing

--- extension/content_script.js
+++ extension/content_script.js
@@ -5,7 +5,8 @@ for (var j = 0; j < text.length; j++) {
     if (text[j].innerHTML.includes('mask')) {
         var injection = '<a href="https://lepips.github.io/WebMed/" style="background-color: orange">mask<a>';
         text[j].innerHTML = text[j].innerHTML.replace('mask', injection)
-    } else if (text[j].innerHTML.includes('social distance')) {
-        
+    } else if (text[j].innerHTML.toLowerCase().includes('social distancing')) {
+        var injection = '<a href="https://lepips.github.io/WebMed/" style="background-color: orange">social distancing<a>';
+        text[j].innerHTML = text[j].innerHTML.replace('social distancing', injection)
     }
 }
